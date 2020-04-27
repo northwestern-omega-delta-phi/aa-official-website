edbd8773bba69b1242821316eed3e602ee335196
javiercuadra <javiercuadra2020@u.northwestern.edu>
2020-04-26 21:21:20 -0400

    css-variables for color palette & import Open Sans font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<p className='test'>Open Sans Regular</p>
+		</div>
+	);
 }
 
 export default App;
