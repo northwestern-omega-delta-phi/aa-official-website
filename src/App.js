0c7883573e53a77f5bcf1df5d7b2df530f148559
Javier Francisco Cuadra <javiercuadra2020@u.northwestern.edu>
2020-05-09 19:39:27 -0400

    CSS Color Palette [ch29] [ch30] (#2)
    * css-variables for color palette & import Open Sans font
    
    * add colors to palette
    
    * updated color palette
    
    * added final color palette
    
    * made font import smaller

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './App.css';
 import './normalize.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<p className='test'></p>
+		</div>
+	);
 }
 
 export default App;
