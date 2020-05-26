031f4b888f1b6ae27b222a91bb9ff569f0676031
javiercuadra <javiercuadra2020@u.northwestern.edu>
2020-05-25 20:18:09 -0400

    organized and cleaned files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.min.css";
+
+import NavBar from "./scripts/components/NavBar";
+// import MainContent from "./components/MainContent";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      {/* <MainContent /> */}
+    </div>
+  );
+}
+
+export default App;
--- src/scripts/components/NavBar.js
+++ src/scripts/components/NavBar.js
@@ -1,60 +1,60 @@
-import React from 'react';
+import React from "react";
 
-import NavItem from './NavItem.js';
+import NavItem from "./NavItem.js";
 
-import Logo from '../../assets/logo';
-import { ReactComponent as KnightIcon } from '../../assets/knight.svg';
-import { ReactComponent as DonateIcon } from '../../assets/donate.svg';
-import { ReactComponent as AboutIcon } from '../../assets/about.svg';
-import { ReactComponent as BrothersIcon } from '../../assets/brothers.svg';
-import { ReactComponent as LoginIcon } from '../../assets/login.svg';
+import Logo from "../../assets/images/logo";
+import { ReactComponent as KnightIcon } from "../../assets/images/knight.svg";
+import { ReactComponent as DonateIcon } from "../../assets/images/donate.svg";
+import { ReactComponent as AboutIcon } from "../../assets/images/about.svg";
+import { ReactComponent as BrothersIcon } from "../../assets/images/brothers.svg";
+import { ReactComponent as LoginIcon } from "../../assets/images/login.svg";
 
 function NavBar() {
-	return (
-		<nav className='navbar'>
-			<ul className='navbar__list'>
-				<ul className='navbar__section navbar__section--left'>
-					<li className='navbar__sub-section navbar__sub-section--left'>
-						<NavItem
-							linkName='Omega Delta Phi - AA Chapter'
-							itemModifier=' navbar__nav-item--logo'
-							linkTextModifier=' navbar__link-text--logo'
-							icon={
-								<Logo className='navbar__link-icon navbar__link-icon--logo' />
-							}
-						/>
-					</li>
-					<li className='navbar__sub-section navbar__sub-section--right'>
-						<NavItem
-							linkName='About'
-							icon={<AboutIcon className='navbar__link-icon' />}
-						/>
-						<NavItem
-							linkName='Brothers'
-							icon={<BrothersIcon className='navbar__link-icon' />}
-						/>
-						<NavItem
-							linkName='Become a Knight'
-							icon={<KnightIcon className='navbar__link-icon' />}
-						/>
-						<NavItem
-							linkName='Donate'
-							icon={<DonateIcon className='navbar__link-icon' />}
-						/>
-					</li>
-				</ul>
-				<li className='navbar__section navbar__section--right'>
-					<NavItem
-						linkName='Login'
-						linkTextModifier=' navbar__link-text--login'
-						icon={
-							<LoginIcon className='navbar__link-icon navbar__link-icon--login' />
-						}
-					/>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <ul className="navbar__list">
+        <ul className="navbar__section navbar__section--left">
+          <li className="navbar__sub-section navbar__sub-section--left">
+            <NavItem
+              linkName="Omega Delta Phi - AA Chapter"
+              itemModifier=" navbar__nav-item--logo"
+              linkTextModifier=" navbar__link-text--logo"
+              icon={
+                <Logo className="navbar__link-icon navbar__link-icon--logo" />
+              }
+            />
+          </li>
+          <li className="navbar__sub-section navbar__sub-section--right">
+            <NavItem
+              linkName="About"
+              icon={<AboutIcon className="navbar__link-icon" />}
+            />
+            <NavItem
+              linkName="Brothers"
+              icon={<BrothersIcon className="navbar__link-icon" />}
+            />
+            <NavItem
+              linkName="Become a Knight"
+              icon={<KnightIcon className="navbar__link-icon" />}
+            />
+            <NavItem
+              linkName="Donate"
+              icon={<DonateIcon className="navbar__link-icon" />}
+            />
+          </li>
+        </ul>
+        <li className="navbar__section navbar__section--right">
+          <NavItem
+            linkName="Login"
+            linkTextModifier=" navbar__link-text--login"
+            icon={
+              <LoginIcon className="navbar__link-icon navbar__link-icon--login" />
+            }
+          />
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
