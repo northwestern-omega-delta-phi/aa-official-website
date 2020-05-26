9794e8b6abdb07be574424f3cc631ff055e0e0b5
javiercuadra <javiercuadra2020@u.northwestern.edu>
2020-05-26 18:10:18 -0400

    refactored and fixed form bugs

--- src/scripts/components/Form.js
+++ src/scripts/components/Form.js
@@ -1,166 +1,117 @@
-import React from 'react';
+import React from "react";
 
 function Form() {
-	return (
-		<figure className='recruitment-page__form-figure'>
-			<figcaption>
-				<h3 className='recruitment-page__form-header--long'>
-					Fill out this form if you are interested!
-				</h3>
-				<h3 className='recruitment-page__form-header'>Interested?</h3>
-			</figcaption>
-			<form className='recruitment-page__form'>
-				<div className='recruitment-page__input-field'>
-					<input
-						className='recruitment-page__input'
-						id='name'
-						name='name'
-						required
-						type='text'
-					/>
-					<label
-						className='recruitment-page__label recruitment-page__label--large'
-						for='name'>
-						Please enter your Full Name
-					</label>
-					<label
-						className='recruitment-page__label recruitment-page__label'
-						for='name'>
-						Full Name
-					</label>
-					<span className='recruitment-page__input-underline'></span>
-				</div>
-				<div className='recruitment-page__input-field'>
-					<input
-						className='recruitment-page__input'
-						id='year'
-						name='year'
-						required
-						type='number'
-					/>
-					<label
-						className='recruitment-page__label recruitment-page__label--large'
-						for='year'>
-						Please enter your School Year
-					</label>
-					<label
-						className='recruitment-page__label recruitment-page__label'
-						for='year'>
-						School Year
-					</label>
-					<span className='recruitment-page__input-underline'></span>
-				</div>
-				<div className='recruitment-page__input-field'>
-					<input
-						className='recruitment-page__input'
-						id='email'
-						name='email'
-						required
-						type='email'
-					/>
-					<label
-						className='recruitment-page__label recruitment-page__label--large'
-						for='email'>
-						Please enter your Email
-					</label>
-					<label
-						className='recruitment-page__label recruitment-page__label'
-						for='email'>
-						Email
-					</label>
-					<span className='recruitment-page__input-underline'></span>
-				</div>
-				<div className='recruitment-page__input-field'>
-					<input
-						className='recruitment-page__input'
-						id='major'
-						name='major'
-						required
-						type='text'
-					/>
-					<label
-						className='recruitment-page__label recruitment-page__label--large'
-						for='major'>
-						Please enter your Major
-					</label>
-					<label
-						className='recruitment-page__label recruitment-page__label'
-						for='major'>
-						Major
-					</label>
-					<span className='recruitment-page__input-underline'></span>
-				</div>
-				<div className='recruitment-page__input-field'>
-					<input
-						className='recruitment-page__input'
-						id='fun-fact'
-						name='fun-fact'
-						required
-						type='text'
-					/>
-					<label
-						className='recruitment-page__label recruitment-page__label--large'
-						for='fun-fact'>
-						Please enter a Fun Fact About Yourself
-					</label>
-					<label
-						className='recruitment-page__label recruitment-page__label'
-						for='fun-fact'>
-						Fun Fact About Yourself
-					</label>
-					<span className='recruitment-page__input-underline'></span>
-				</div>
+  return (
+    <figure className="recruitment-page__form-figure">
+      <figcaption>
+        <h3 className="recruitment-page__form-header--long">
+          Fill out this form if you are interested!
+        </h3>
+        <h3 className="recruitment-page__form-header">Interested?</h3>
+      </figcaption>
+      <form className="recruitment-page__form">
+        <div className="recruitment-page__input-field">
+          <input
+            className="recruitment-page__input"
+            id="name"
+            name="name"
+            required
+            type="text"
+          />
+          <label
+            className="recruitment-page__label recruitment-page__label--large"
+            htmlFor="name"
+          >
+            Please enter your Full Name
+          </label>
+          <label className="recruitment-page__label" htmlFor="name">
+            Full Name
+          </label>
+          <span className="recruitment-page__input-underline"></span>
+        </div>
+        <div className="recruitment-page__input-field">
+          <input
+            className="recruitment-page__input"
+            id="year"
+            name="year"
+            required
+            type="number"
+          />
+          <label
+            className="recruitment-page__label recruitment-page__label--large"
+            htmlFor="year"
+          >
+            Please enter your School Year
+          </label>
+          <label className="recruitment-page__label" htmlFor="year">
+            School Year
+          </label>
+          <span className="recruitment-page__input-underline"></span>
+        </div>
+        <div className="recruitment-page__input-field">
+          <input
+            className="recruitment-page__input"
+            id="email"
+            name="email"
+            required
+            type="email"
+          />
+          <label
+            className="recruitment-page__label recruitment-page__label--large"
+            htmlFor="email"
+          >
+            Please enter your Email
+          </label>
+          <label className="recruitment-page__label" htmlFor="email">
+            Email
+          </label>
+          <span className="recruitment-page__input-underline"></span>
+        </div>
+        <div className="recruitment-page__input-field">
+          <input
+            className="recruitment-page__input"
+            id="major"
+            name="major"
+            required
+            type="text"
+          />
+          <label
+            className="recruitment-page__label recruitment-page__label--large"
+            htmlFor="major"
+          >
+            Please enter your Major
+          </label>
+          <label className="recruitment-page__label" htmlFor="major">
+            Major
+          </label>
+          <span className="recruitment-page__input-underline"></span>
+        </div>
+        <div className="recruitment-page__input-field">
+          <input
+            className="recruitment-page__input"
+            id="fun-fact"
+            name="fun-fact"
+            required
+            type="text"
+          />
+          <label
+            className="recruitment-page__label recruitment-page__label--large"
+            htmlFor="fun-fact"
+          >
+            Please enter a Fun Fact About Yourself
+          </label>
+          <label className="recruitment-page__label" htmlFor="fun-fact">
+            Fun Fact About Yourself
+          </label>
+          <span className="recruitment-page__input-underline"></span>
+        </div>
 
-				<button type='submit' className='recruitment-page__submit-button'>
-					Submit
-				</button>
-			</form>
-		</figure>
-	);
+        <button type="submit" className="recruitment-page__submit-button">
+          Submit
+        </button>
+      </form>
+    </figure>
+  );
 }
 
 export default Form;
-
-// export function FormSmall() {
-// 	return (
-// 		<figure className='form-small'>
-// 			<figcaption>
-// 				<h3 className='screen-is-above-416px-text'>
-// 					Fill out this form if you are interested!
-// 				</h3>
-// 				<h3 className='screen-is-below-416px-text'>Interested?</h3>
-// 			</figcaption>
-// 			<form>
-// 				<div className='input-field'>
-// 					<input id='name' name='name' required type='text' />
-// 					<label for='name'>Full Name</label>
-// 					<span></span>
-// 				</div>
-// 				<div className='input-field'>
-// 					<input id='year' name='year' required type='number' />
-// 					<label for='year'>School Year</label>
-// 					<span></span>
-// 				</div>
-// 				<div className='input-field'>
-// 					<input id='email' name='email' required type='email' />
-// 					<label for='email'>Email</label>
-// 					<span></span>
-// 				</div>
-// 				<div className='input-field'>
-// 					<input id='major' name='major' required type='text' />
-// 					<label for='major'>Major</label>
-// 					<span></span>
-// 				</div>
-// 				<div className='input-field'>
-// 					<input id='fun-fact' name='fun-fact' required type='text' />
-// 					<label for='fun-fact'>Fun Fact About Yourself</label>
-// 					<span></span>
-// 				</div>
-
-// 				<button type='submit' className='submit-button'>
-// 					Submit
-// 				</button>
-// 			</form>
-// 		</figure>
-// 	);
-// }
--- src/scripts/components/RecruitmentPageHeader.js
+++ src/scripts/components/RecruitmentPageHeader.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function RecruitmentPageHeader() {
-	return (
-		<header className='recruitment-page__header'>
-			<iframe
-				className='recruitment-page__video'
-				title='Become a Knight of Omega Delta Phi'
-				width='900'
-				height='675'
-				src='https://www.youtube.com/embed/UNZqm3dxd2w?autoplay=1'
-				frameborder='0'
-				allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-				allowfullscreen></iframe>
-		</header>
-	);
+  return (
+    <header className="recruitment-page__header">
+      <iframe
+        className="recruitment-page__video"
+        title="Become a Knight of Omega Delta Phi"
+        width="900"
+        height="675"
+        src="https://www.youtube.com/embed/UNZqm3dxd2w"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </header>
+  );
 }
 
 export default RecruitmentPageHeader;
