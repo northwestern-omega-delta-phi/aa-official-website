d18ac671f81fbd99a62836dc8da7bd6522baf2d1
Javier Francisco Cuadra <javiercuadra2020@u.northwestern.edu>
2020-06-08 19:27:09 -0400

    Header Component [ch58] (#12)
    * sidebar nav
    
    * sidebar nav overhaul
    
    * altered header font
    
    * cleaned code
    
    * basic layout
    
    * reorganized files
    
    * made header responsive
    
    * refactored into SASS
    
    * trashed unneeded files
    
    * added odp crest
    
    * organized and cleaned files
    
    * refactored styles
    
    * refactored variables file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import './App.css';
-import './normalize.css';
+import React from "react";
+import "./App.min.css";
+
+import NavBar from "./scripts/components/NavBar";
+// import MainContent from "./components/MainContent";
 
 function App() {
-	return (
-		<div className='App'>
-			<p className='test'>Hello, world!</p>
-		</div>
-	);
+  return (
+    <div className="App">
+      <NavBar />
+      {/* <MainContent /> */}
+    </div>
+  );
+
 }
 
 export default App;
